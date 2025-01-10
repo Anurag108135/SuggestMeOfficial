29d13f56cf895f734a65b8d343e4fc2faf699eac
Ajeet <ajeet.kumar@graspcorn.com>
2025-01-10 07:59:25 -0500

    commint

--- SuggestMeWebApp/obj/Debug/net8.0/SuggestMeWebApp.AssemblyInfo.cs
+++ SuggestMeWebApp/obj/Debug/net8.0/SuggestMeWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuggestMeWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bf7e285ce529086925f4ca9b558774090c44677")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuggestMeWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuggestMeWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
